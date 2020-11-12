06217e472063a1ac13938c71aadb15e6ba87eec2
rayqian <ray.qian@hotmail.com>
2020-11-12 16:56:09 -0600

    add the location code that determines the device's location

--- app/src/main/java/com/example/myknowyourgovermentapp/MainActivity.java
+++ app/src/main/java/com/example/myknowyourgovermentapp/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.myknowyourgovermentapp;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -9,15 +17,24 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.io.IOError;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
 public class MainActivity extends AppCompatActivity
         implements View.OnClickListener{
@@ -25,6 +42,10 @@ public class MainActivity extends AppCompatActivity
     private RecyclerView recyclerView;
     private OfficialAdapter myAdapter;
     private final List<Official> officialList = new ArrayList<>();
+
+    private LocationManager locationManager;
+    private Criteria criteria;
+    private static int MY_LOCATION_REQUEST_CODE_ID = 111;
 
     private static final String TAG = "from MainActivity";
 
@@ -48,6 +69,27 @@ public class MainActivity extends AppCompatActivity
         Official b = new Official("vice president", "Baiden", "Dom");
         officialList.add(a);
         officialList.add(b);
+
+        //location code
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        criteria = new Criteria();
+
+        criteria.setPowerRequirement(Criteria.POWER_HIGH);
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setSpeedRequired(false);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                    },
+                    MY_LOCATION_REQUEST_CODE_ID);
+        } else {
+            setLocation();
+        }
 
     }
 
@@ -109,13 +151,60 @@ public class MainActivity extends AppCompatActivity
             public void onClick(DialogInterface dialogInterface, int i) {
                 String user_input = et.getText().toString();
 //                handleStockSearch(user_input);
-
             }
         });
-
         builder.setTitle("Enter a City, State or a Zip Code:");
 
         AlertDialog dialog = builder.create();
         dialog.show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull
+            String[] permissions, @NonNull
+                    int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == MY_LOCATION_REQUEST_CODE_ID) {
+            if (permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION) &&
+                    grantResults[0] == PERMISSION_GRANTED) {
+                setLocation();
+                return;
+            }
+        }
+        ((TextView) findViewById(R.id.location)).setText("No permission");
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private void setLocation() {
+        //best provider: gps, network or passive
+        String bestProvider = locationManager.getBestProvider(criteria, true);
+//        ((TextView) findViewById(R.id.location)).setText(bestProvider);
+
+        Location currentLocation = null;
+        if (bestProvider != null) {
+            currentLocation = locationManager.getLastKnownLocation(bestProvider);
+        }
+        if (currentLocation != null) {
+            Geocoder geocoder = new Geocoder(this);
+            try{
+                List<Address> adrs = geocoder.getFromLocation(currentLocation.getLatitude(), currentLocation.getLongitude(), 1);
+//                StringBuilder sb = new StringBuilder();
+//                Address adr = adrs.get(0);
+//                sb.append(adr.getLocality()).append(", ").append(adr.getAdminArea()).append(adr.get)
+                ((TextView) findViewById(R.id.location)).setText(adrs.get(0).getAddressLine(0));
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        } else {
+            ((TextView) findViewById(R.id.location)).setText("Location Unavailable");
+        }
+
+
+
+
     }
 }
